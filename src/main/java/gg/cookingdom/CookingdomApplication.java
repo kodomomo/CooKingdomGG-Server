cf59122a9c8461450b37b18f31dc65b16b7bd740
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-03-14 21:39:07 +0900

    [추가] 스프링 프로젝트 init

---
+++ src/main/java/gg/cookingdom/CookingdomApplication.java
@@ -0,0 +1,13 @@
+package gg.cookingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingdomApplication.class, args);
+    }
+
+}
