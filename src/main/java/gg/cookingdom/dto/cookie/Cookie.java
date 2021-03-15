91c852aa6a9688a2c32c0311a20dfcb75d5835da
Ryua <machiro119@naver.com>
2021-03-16 00:39:58 +0900

    [delete] - delete final

--- src/main/java/gg/cookingdom/dto/cookie/Cookie.java
+++ src/main/java/gg/cookingdom/dto/cookie/Cookie.java
@@ -4,16 +4,20 @@ import gg.cookingdom.enums.CookieMajor;
 import gg.cookingdom.enums.CookiePosition;
 import gg.cookingdom.enums.Rank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Cookie {
-    private final String name;
-    private final Rank rank;
-    private final Integer tier;
-    private final CookieMajor major;
-    private final CookiePosition position;
-    private final String topping;
-    private final String description;
+    private String name;
+    private Rank rank;
+    private Integer tier;
+    private CookieMajor major;
+    private CookiePosition position;
+    private String topping;
+    private String description;
 }
