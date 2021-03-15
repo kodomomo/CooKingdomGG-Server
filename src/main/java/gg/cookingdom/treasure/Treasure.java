397db3b9b0b1c52331c57c1bcb7e93ef1a98dbd2
Ryua <machiro119@naver.com>
2021-03-15 22:18:02 +0900

    [FIX] - 롬복

--- src/main/java/gg/cookingdom/treasure/Treasure.java
+++ src/main/java/gg/cookingdom/treasure/Treasure.java
@@ -1,17 +1,14 @@
 package gg.cookingdom.treasure;
 
 import gg.cookingdom.cookie.Rank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class Treasure {
     private final String name;
     private final Rank rank;
     private final String effect;
     private final String description;
-
-    public Treasure(String name, Rank rank, String effect, String description) {
-        this.name = name;
-        this.rank = rank;
-        this.effect = effect;
-        this.description = description;
-    }
 }
