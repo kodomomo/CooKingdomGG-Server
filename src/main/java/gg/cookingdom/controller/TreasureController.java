cfbac583798a3e6545508ee27976c96dc071192d
Ryua <machiro119@naver.com>
2021-03-16 00:39:33 +0900

    [FIX] - contoller -> rest controller

--- src/main/java/gg/cookingdom/controller/TreasureController.java
+++ src/main/java/gg/cookingdom/controller/TreasureController.java
@@ -4,14 +4,14 @@ import gg.cookingdom.dto.treasure.Treasure;
 import gg.cookingdom.enums.Rank;
 import gg.cookingdom.repository.treasure.TreasureInfoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/treasure")
 @RequiredArgsConstructor
 public class TreasureController {
