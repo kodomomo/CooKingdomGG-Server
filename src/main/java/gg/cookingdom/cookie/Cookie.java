6c744ae031791a8cd306637894afab2b5ef1b0eb
Ryua <machiro119@naver.com>
2021-03-15 22:18:04 +0900

    [FIX] - 롬복

--- src/main/java/gg/cookingdom/cookie/Cookie.java
+++ src/main/java/gg/cookingdom/cookie/Cookie.java
@@ -1,5 +1,10 @@
 package gg.cookingdom.cookie;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Cookie {
     private final String name;
     private final Rank rank;
@@ -8,14 +13,4 @@ public class Cookie {
     private final CookiePosition position;
     private final String topping;
     private final String description;
-
-    public Cookie(String name, Rank rank, Integer tier, CookieMajor major, CookiePosition position, String topping, String description) {
-        this.name = name;
-        this.rank = rank;
-        this.tier = tier;
-        this.major = major;
-        this.position = position;
-        this.topping = topping;
-        this.description = description;
-    }
 }
