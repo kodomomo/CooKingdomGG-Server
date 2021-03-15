4fe97a4ded0c5bf143a96e5b2bd404e345980bc4
Ryua <machiro119@naver.com>
2021-03-15 22:18:27 +0900

    [ADD] - in memory database

--- src/main/java/gg/cookingdom/cookie/JsonCookieInfoRepository.java
+++ src/main/java/gg/cookingdom/cookie/JsonCookieInfoRepository.java
@@ -1,19 +1,43 @@
 package gg.cookingdom.cookie;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Repository;
 
+import java.io.FileReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class JsonCookieInfoRepository implements CookieInfoRepository {
+
+    private final List<Cookie> cookies;
+
+    @SneakyThrows
+    public JsonCookieInfoRepository() {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(
+                getClass().getResource("static/cookieinfo.json").getPath()));
+
+        JSONObject jsonObject = (JSONObject) obj;
+        ObjectMapper mapper = new ObjectMapper();
+        this.cookies = mapper.readValue(jsonObject.toJSONString(), List.class);
+    }
+
     @Override
-    public Cookie getCookieByName(String name) {
-        return null;
+    public List<Cookie> getCookieByName(String name) {
+        return cookies.stream()
+                .filter(cookie -> cookie.getName().contains(name))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Cookie> getCookieByRank(Rank rank) {
-        return null;
+        return cookies.stream()
+                .filter(cookie -> cookie.getRank() == rank)
+                .collect(Collectors.toList());
     }
 
     @Override
