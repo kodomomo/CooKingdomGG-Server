5f553036bee18be0e10a44d3ecce60508118a99e
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-03-18 14:56:05 +0900

    [수정] Spring Security Cors 재설정
    Security Cors Config 수정 및 WebConfig 삭제

--- src/main/java/gg/cookingdom/config/SecurityConfig.java
+++ src/main/java/gg/cookingdom/config/SecurityConfig.java
@@ -1,43 +1,53 @@
 package gg.cookingdom.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
                 .cors().and()
-                .formLogin().disable()
+                .formLogin()
+                    .disable()
                 .headers()
-                .frameOptions().disable().and()
+                .frameOptions()
+                    .disable().and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/cookie/**").permitAll()
-                .antMatchers("/treasure/**").permitAll()
-                .antMatchers("/combination/**").permitAll()
-                .antMatchers("/image/**").permitAll()
+                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                    .antMatchers("/cookie/**").permitAll()
+                    .antMatchers("/treasure/**").permitAll()
+                    .antMatchers("/combination/**").permitAll()
+                    .antMatchers("/image/**").permitAll()
                 .anyRequest().authenticated();
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("8000")
-                .allowedHeaders("*");
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("GET, POST, PUT, PATCH");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
