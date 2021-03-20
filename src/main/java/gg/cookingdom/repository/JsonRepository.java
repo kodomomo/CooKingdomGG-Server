b7f1b02856a1569ed71acf2f1db12d22d8e9679e
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-03-21 04:43:00 +0900

    [EDIT] - Change JsonRepository return type

--- src/main/java/gg/cookingdom/repository/JsonRepository.java
+++ src/main/java/gg/cookingdom/repository/JsonRepository.java
@@ -4,22 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 @Component
 public class JsonRepository {
 
     @SneakyThrows
-    public List<LinkedHashMap<String, ?>>  getJsonInfo(String path) throws IOException, ParseException {
+    public List getJsonInfo(String path) {
         JSONParser parser = new JSONParser();
         ObjectMapper mapper = new ObjectMapper();
 
