9c3dd2edc8517ed278599713c95b44dcf986d5aa
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-03-31 09:14:48 +0900

    [DEBUG] -

--- src/main/java/gg/cookingdom/config/CorsConfigurer.java
+++ src/main/java/gg/cookingdom/config/CorsConfigurer.java
@@ -3,13 +3,14 @@ package gg.cookingdom.config;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class CorsConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     @Override
     public void configure(HttpSecurity http) {
         CorsFilter corsFilter = new CorsFilter();
-        http.addFilter(corsFilter);
+        http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
