2bcbb6987e2cd12ea963802d97299ea32b750a56
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-06 15:05:43 +0900

    [ADD] - Connect image controller

---
+++ src/main/java/gg/cookingdom/controller/ImageController.kt
@@ -0,0 +1,22 @@
+package gg.cookingdom.controller
+
+import gg.cookingdom.service.image.ImageService
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/image")
+class ImageController(
+    private val imageService: ImageService
+) {
+    @GetMapping(
+        value = ["/{imageName}"],
+        produces = [MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE]
+    )
+    fun getImagesByName(@PathVariable imageName: String): ByteArray {
+        return imageService.getImage(imageName)
+    }
+}
